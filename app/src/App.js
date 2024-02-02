d081bf128091b5d94b57729c5762a2eee6f675a7
cocoachou <yujinlee0131@gmail.com>
2024-02-02 13:57:02 +0900

    Rename LoginScreen

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './screens/Login';
+import Login from './screens/LoginScreen';
 
 const App = () => {
   return (
