9cc144f7cdc9e00df4921dfe61cbd79acb7346d2
cocoachou <yujinlee0131@gmail.com>
2024-02-27 14:32:23 +0900

    Update StageLabels

--- app/src/screens/StageScreen.js
+++ app/src/screens/StageScreen.js
@@ -1,18 +1,18 @@
 import { View, ScrollView, StyleSheet, Text } from 'react-native';
 import Stage from '../components/Stage';
 import { useNavigation } from '@react-navigation/native';
+import StageLabels from '../components/StageLabels';
 
 const StageScreen = () => {
   const numberOfStages = 30;
   const navigation = useNavigation();
-  const stageLabels = ['test1','test2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'];
 
   const onPress = (stageNumber, stageLabel) => {
     navigation.navigate('DrawingScreen', { stageLabel });
   };
 
   const stageArray = Array.from({ length: numberOfStages }, (_, index) => {
-    const stageLabel = stageLabels[index]; // stageNumber와 동일한 인덱스의 stageLabel을 가져옴
+    const stageLabel = StageLabels[index]; // stageNumber와 동일한 인덱스의 stageLabel을 가져옴
     return (
       <Stage key={index + 1} stageNumber={index + 1} stageLabel={stageLabel} onPress={onPress} />
     );
