e34dc8155cfb4d7432b559eff037fb77a879e2f6
cocoachou <yujinlee0131@gmail.com>
2024-02-28 12:21:35 +0900

    Fix DrawingScreen

--- app/src/screens/StageScreen.js
+++ app/src/screens/StageScreen.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { View, ScrollView, StyleSheet, Text } from 'react-native';
 import Stage from '../components/Stage';
 import { useNavigation } from '@react-navigation/native';
@@ -6,33 +7,39 @@ import StageLabels from '../components/StageLabels';
 const StageScreen = () => {
   const numberOfStages = 30;
   const navigation = useNavigation();
-  const [completedStages, setCompletedStages] = React.useState(Array(numberOfStages).fill(false));
+  const [completedStages, setCompletedStages] = React.useState(
+    Array(numberOfStages).fill(false)
+  );
 
   const onPress = (stageNumber, stageLabel) => {
-    navigation.navigate('DrawingScreen', { 
-      stageNumber: stageNumber, 
-      stageLabel: stageLabel, 
-      setCompletedStages: setCompletedStages 
+    navigation.navigate('DrawingScreen', {
+      stageNumber: stageNumber,
+      stageLabel: stageLabel,
+      completedStages: completedStages,
     });
   };
 
   const stageArray = Array.from({ length: numberOfStages }, (_, index) => {
     const stageLabel = StageLabels[index]; // stageNumber와 동일한 인덱스의 stageLabel을 가져옴
     return (
-      <Stage key={index + 1} stageNumber={index + 1} stageLabel={stageLabel} onPress={onPress} />
+      <Stage
+        key={index + 1}
+        stageNumber={index + 1}
+        stageLabel={stageLabel}
+        onPress={onPress}
+      />
     );
   });
-  
 
   return (
     <View>
       <Text style={styles.title}>Stage</Text>
       <ScrollView contentContainerStyle={styles.stageContainer}>
-      {stageArray.map((stage, index) => (
+        {stageArray.map((stage, index) => (
           <Stage
             key={index + 1}
             stageNumber={index + 1}
-            stageLabel={stageLabels[index]}
+            stageLabel={StageLabels[index]}
             onPress={onPress}
             isCompleted={completedStages[index]}
           />
