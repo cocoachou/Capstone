653a60f0cc01ae440f13c33031e6afc7a620b1da
usub532 <132990484+usub532@users.noreply.github.com>
2024-04-22 19:49:52 +0900

    Update StageScreen.js

--- app/src/screens/StageScreen.js
+++ app/src/screens/StageScreen.js
@@ -11,7 +11,24 @@ const StageScreen = () => {
     Array(numberOfStages).fill(false)
   );
 
+  const [lockedStages, setLockedStages] = React.useState(Array(numberOfStages).fill(true).map((_, index) => index > 0)); // 첫 번째 스테이지만 잠금 해제
+
+
   const onPress = (stageNumber, stageLabel) => {
+    const updatedCompletedStages = [...completedStages];
+    updatedCompletedStages[stageNumber - 1] = true;
+    setCompletedStages(updatedCompletedStages);
+    const updatedLockedStages = [...lockedStages];
+    if (stageNumber < numberOfStages) {
+      updatedLockedStages[stageNumber] = false; // 현재 스테이지의 다음 스테이지 잠금 해제
+    }
+    setLockedStages(updatedLockedStages);
+    navigation.navigate('DrawingScreen', {
+      stageNumber: stageNumber,
+      stageLabel: stageLabel,
+      completedStages: updatedCompletedStages,
+      setCompletedStages: setCompletedStages,
+    });
     navigation.navigate('DrawingScreen', {
       stageNumber: stageNumber,
       stageLabel: stageLabel,
@@ -20,6 +37,8 @@ const StageScreen = () => {
     });
   };
 
+
+  
   const stageArray = Array.from({ length: numberOfStages }, (_, index) => {
     const stageLabel = StageLabels[index]; // stageNumber와 동일한 인덱스의 stageLabel을 가져옴
     return (
@@ -34,15 +53,16 @@ const StageScreen = () => {
 
   return (
     <View>
-      <Text style={styles.title}>Stage</Text>
+       <Text style={styles.title}>Stage</Text>
       <ScrollView contentContainerStyle={styles.stageContainer}>
-        {stageArray.map((stage, index) => (
+        {Array.from({ length: numberOfStages }, (_, index) => (
           <Stage
             key={index + 1}
             stageNumber={index + 1}
             stageLabel={StageLabels[index]}
             onPress={onPress}
             isCompleted={completedStages[index]}
+            isLocked={lockedStages[index]} // 잠금 상태 prop 전달
           />
         ))}
       </ScrollView>
